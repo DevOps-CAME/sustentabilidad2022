8edb7ed8fa21e93c4961c08c96321bdf8dfb6c1d
ggracis <gastongracis@gmail.com>
2023-02-13 17:03:18 -0300

    otros files

---
+++ indicadores.php
@@ -0,0 +1,91 @@
+<?php include "head.php"; ?>
+
+<body class="stretched">
+
+    <!-- Document Wrapper
+	============================================= -->
+    <div id="wrapper" class="clearfix">
+
+        <?php include "header.php"; ?>
+
+
+        <div class="section parallax mb-0 mt-0 d-flex align-items-center medio-ambiente portada">
+            <div class="container clearfix">
+                <div class="arr-der">
+                    <img id="portada-mundito" src="images/portada_mundito.png" width="100%">
+                </div>
+                <div class="arr-izk">
+                    <img id="portada-blanco" src="images/portada_blanco.gif" width="100%">
+                </div>
+                <div>
+                    <h2 class="titulo-seccion" style="color: #fff;">Indicadores</h2>
+                </div>
+            </div>
+        </div>
+
+
+        <!-- indicadores
+		============================================= -->
+        <section id="indicadores">
+            <div class="content-wrap">
+                <div class="container clearfix">
+
+
+                    <div class="col-12 ml-1">
+                        <div class="row">
+                            <div class="col-1 p-0"><a href="https://www.un.org/sustainabledevelopment/es/hunger/"
+                                    target="_blank"><img src="images/ods/02.png" alt="Image"></a></div>
+                            <div class="col-1 p-0"><a
+                                    href="https://www.un.org/sustainabledevelopment/es/infrastructure/"
+                                    target="_blank"><img src="images/ods/09.png" alt="Image"></a></div>
+                            <div class="col-1 p-0"><a href="https://www.un.org/sustainabledevelopment/es/cities/"
+                                    target="_blank"><img src="images/ods/11.png" alt="Image"></a></div>
+                            <div class="col-1 p-0"><a
+                                    href="https://www.un.org/sustainabledevelopment/es/sustainable-consumption-production/"
+                                    target="_blank"><img src="images/ods/12.png" alt="Image"></a></div>
+
+                        </div>
+                    </div>
+                    <section id="medio-ambiente">
+                        <div class="col-12 mt-4 text-justify">
+                            <p>
+                                En CAME entendemos la importancia del triple impacto (económico, social
+                                y ambiental), y por ello acompañamos —a través de los sectores y
+                                departamento que conforman nuestra institución— a las entidades, micro,
+                                pequeñas y medianas empresas en la adopción de estrategias para la correcta
+                                implementación de este paradigma en su labor cotidiana.
+
+                            </p>
+                        </div>
+                    </section>
+                </div>
+
+
+            </div>
+
+            <!-- #content end -->
+        </section>
+
+        <?php include "footer.php" ?>
+
+
+    </div><!-- #wrapper end -->
+
+    <!-- Go To Top
+============================================= -->
+    <div id="gotoTop" class="icon-double-angle-up bg-white text-dark rounded-circle shadow"></div>
+
+    <!-- External JavaScripts
+============================================= -->
+    <script src="js/jquery.js"></script>
+    <script src="js/plugins.min.js"></script>
+
+    <!-- Footer Scripts
+============================================= -->
+    <script src="js/functions.js"></script>
+
+
+
+</body>
+
+</html>
